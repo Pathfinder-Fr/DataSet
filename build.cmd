110242da61801bbce57845b65ac38b689a4cfe66
Thomas Bolon <tbolon@mb3m.fr>
2015-02-14 12:58:42 +0100

    reorganize build

--- build.cmd
+++ build.cmd
@@ -1,4 +1,16 @@
-if "%~1"=="" build build.proj Build
-if "%~2"=="" build %~1 Build
-msbuild /t:%~2 %~1 /fl /flp:logfile=build.log
+if exist "%ProgramFiles(x86)%\MSBuild\12.0\bin" goto msbuildx86
+if exist "%ProgramFiles%\MSBuild\12.0\bin" goto msbuild
 
+echo "Unable to detect suitable environment. Check if msbuild is installed."
+exit 1
+
+:msbuildx86
+set msbuildpath=%ProgramFiles(x86)%\MSBuild\12.0\bin\
+goto build
+
+:msbuild
+set msbuildpath=%ProgramFiles%\MSBuild\12.0\bin\
+goto build
+
+:build
+"%msbuildpath%msbuild.exe" /t:Build build\build.proj /fl /flp:logfile=build.log
